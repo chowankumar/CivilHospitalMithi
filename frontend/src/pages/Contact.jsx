fa2952d9f40e06f844c380073bade012edd6af23
chowan kumar <chowanmalhani5@gmail.com>
2025-03-16 16:04:41 +0500

    edit contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,34 +1,39 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from 'react';
+import { FaPhone, FaEnvelope, FaMapMarkerAlt, FaBriefcase } from 'react-icons/fa';
 
 const Contact = () => {
   return (
     <div>
-
-
-      <div className='text-center text-2xl pt-10text-gray-500'>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
         <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
       </div>
 
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
-
-        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
-
-        <div className='flex flex-col justify-center items-start gap-6'>
-          <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
-          <p className='text-gray-500'>00000 Willms Station <br />
-            Suite 000, Washington, USA</p>
+        
+        {/* Embedded Google Map for Mithi Civil Hospital */}
+        <iframe
+          className='w-full md:w-1/2 h-80 rounded-lg shadow-lg'
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3620.7451437070786!2d69.80505957532822!3d24.7430829780107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3942e25e8b6e6ff3%3A0x9d78e908c5d2b3b4!2sCivil%20Hospital%20Mithi!5e0!3m2!1sen!2s!4v1710557425672!5m2!1sen!2s'
+          allowFullScreen=''
+          loading='lazy'
+          referrerPolicy='no-referrer-when-downgrade'
+        ></iframe>
+        
+        <div className='w-full md:w-1/2 flex flex-col justify-center items-start gap-6 bg-gray-100 p-6 rounded-lg shadow-lg h-80'>
+          <p className='font-semibold text-lg text-gray-600 flex items-center gap-2'><FaMapMarkerAlt /> OUR OFFICE</p>
+          <p className='text-gray-500'>Civil Hospital Mithi <br />
+            Tharparkar, Sindh, Pakistan</p>
+          
+          <p className='font-semibold text-lg text-gray-600 flex items-center gap-2'><FaPhone /> CONTACT INFO</p>
           <p className='text-gray-500'>Tel: (000) 000-0000 <br />
-            Email: greatstackdev@gmail.com</p>
-          <p className='font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
-          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
-          <button className='border border-black text-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
-
+            <span className='flex items-center gap-2'><FaEnvelope /> Email: info@mithicivilhospital.com</span></p>
+          
+         
+           
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
